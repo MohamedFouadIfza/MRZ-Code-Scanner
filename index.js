1044475d7ba31b55abf4e71ae4e4bd34dea1e023
Mohamed Fouad Ibrahim Mohamed Abdelkhalek <mabdelkhalek@ifza.com>
2024-06-05 16:52:41 +0400

    init to deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3001; // You can change this to any port you prefer
+const port = process.env.PORT || 3001; // You can change this to any port you prefer
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
