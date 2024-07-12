a9284de3c5362b28fd766e841d1ea905ea283b70
Mohamed Fouad Ibrahim Mohamed Abdelkhalek <mabdelkhalek@ifza.com>
2024-07-12 16:40:28 +0400

    handel more erorrs

--- ReadMRZ.js
+++ ReadMRZ.js
@@ -6,39 +6,37 @@ const { readdir } = require('fs-extra')
 const { parentPort } = require("worker_threads");
 
 async function processFile(imagePath) {
+    const parsedPath = parsePath(imagePath);
+    const image = await IJS.load(imagePath);
 
-    try {
-        const parsedPath = parsePath(imagePath);
-        const image = await IJS.load(imagePath);
-
-        const result = await readMrz(image, {
-            debug: false,
-            saveName: join(parsedPath.dir, '../multiMask/', parsedPath.base)
-        });
-        const parsed = parse(result.mrz);
-
-        return parsed
-    } catch (err) {
-        console.log('errrrrrrrrrrrrrr', err)
-    }
+    const result = await readMrz(image, {
+        debug: false,
+        saveName: join(parsedPath.dir, '../multiMask/', parsedPath.base)
+    });
+    const parsed = parse(result.mrz);
+    return parsed
 }
 
 async function readMRZFromImage() {
+    const dirname = resolve('data/imageDir/out/crop');
+    const files = (await readdir(dirname)).filter((f) => {
+        f = f.toLowerCase();
+        return f.endsWith('jpg') || f.endsWith('png') || f.endsWith('jpeg');
+    });
+    const file = files[0]
+    const imagePath = join(dirname, file);
 
-    try {
-        const dirname = resolve('data/imageDir/out/crop');
-        const files = (await readdir(dirname)).filter((f) => {
-            f = f.toLowerCase();
-            return f.endsWith('jpg') || f.endsWith('png') || f.endsWith('jpeg');
-        });
-        const file = files[0]
-        const imagePath = join(dirname, file);
+    const parses = await processFile(imagePath);
+    return parses
 
-        const parses = await processFile(imagePath);
-        parentPort.postMessage({ err: false, ...parses });
-    } catch (er) {
-        parentPort.postMessage({ err: true, errMsg: err });
-    }
 }
 
-readMRZFromImage()
+readMRZFromImage().then((parses) => {
+    parentPort.postMessage({ err: false, ...parses });
+}).catch((err) => {
+    if (err?.toString()) {
+        parentPort.postMessage({ err: true, errMsg: err.toString().replace("Error:", "") });
+    } else {
+        parentPort.postMessage({ err: true, errMsg: err });
+    }
+})
