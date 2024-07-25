bebf8768171b7de2b1ef8b5844085a686910abf4
Mohamed Fouad Ibrahim Mohamed Abdelkhalek <mabdelkhalek@ifza.com>
2024-07-25 09:51:02 +0400

    add assistant code

--- utils/GPT.js
+++ utils/GPT.js
@@ -26,28 +26,28 @@ const monthMap = {
     "OCT": "10",
     "NOV": "11",
     "DEC": "12"
-  };
+};
 
 const extractAndConvertFields = (str) => {
     const fieldPattern = /"(\w+)":\s*"([^"]+)"/g;
     let match;
     const result = {};
-  
+
     while ((match = fieldPattern.exec(str)) !== null) {
-      const [, key, value] = match;
-      if (key === 'birthDate' || key === 'expirationDate') {
-        const dateParts = value.split(' ');
-        const day = dateParts[0].padStart(2, '0');
-        const month = monthMap[dateParts[1]?.toUpperCase()];
-        const year = dateParts[2];
-        result[key] = `${day}-${month}-${year}`;
-      } else {
-        result[key] = value;
-      }
+        const [, key, value] = match;
+        if (key === 'birthDate' || key === 'expirationDate') {
+            const dateParts = value.split(' ');
+            const day = dateParts[0].padStart(2, '0');
+            const month = monthMap[dateParts[1]?.toUpperCase()];
+            const year = dateParts[2];
+            result[key] = `${day}-${month}-${year}`;
+        } else {
+            result[key] = value;
+        }
     }
-  
+
     return result;
-  };
+};
 
 const getPassportDetails = async (base64Image) => {
     const openai = new OpenAI({
@@ -77,6 +77,56 @@ const getPassportDetails = async (base64Image) => {
     return extractFields(response.choices[0].message.content)
 }
 
+
+// const getPassportDetailsAssistant = async (base64Image) => {
+//     const openai = new OpenAI({
+//         apiKey: process.env.NEW_API
+//     })
+
+//     const assistant = await openai.beta.assistants.create({
+//         model: "gpt-4o",
+//         instructions: "you will extract passport information by giving you passport copy and you will use MRZ code or read the information direct from the passport copy if MRZ code unclear, extract all information as JSON only.",
+//         name: "Passport Information Extractor",
+//         response_format: { type: "json_object" }
+//     })
+
+//     const thread = await openai.beta.threads.create()
+    
+
+//     const message = await openai.beta.threads.messages.create(thread.id, {
+//         role: "user",
+//         content: base64Image
+//         // content: [{
+//         //     ty
+//         //     "image_base64": base64Image
+//         //     // type: "image_url",
+//         //     // image_url: {
+//         //     //     url: base64Image
+//         //     // }
+//         // }],
+
+//     })
+
+//     const run = await openai.beta.threads.runs.createAndPoll(
+//         thread.id,
+//         {
+//             assistant_id: assistant.id,
+//         }
+//     );
+
+//     if (run.status === 'completed') {
+//         const messages = await openai.beta.threads.messages.list(
+//             run.thread_id
+//         );
+//         for (const message of messages.data.reverse()) {
+//             console.log(`${message.role} > ${message.content[0].text.value}`);
+//         }
+//     } else {
+//         console.log(run.status);
+//     }
+// }
+
 module.exports = {
     getPassportDetails,
+    // getPassportDetailsAssistant
 }
