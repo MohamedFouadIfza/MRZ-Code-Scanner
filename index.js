3dec5ccb11214740a2b7ca46e70772290a263181
Mohamed Fouad Ibrahim Mohamed Abdelkhalek <mabdelkhalek@ifza.com>
2024-08-02 12:53:14 +0400

    increase express limit

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage })
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 
 
